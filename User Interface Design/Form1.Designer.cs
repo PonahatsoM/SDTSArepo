955a92bbf6246eca7e9e3bc99a29c4a85dc4a259
Ponahatso Moleleki <bohlokwamoleleki@gmail.com>
2026-05-27 13:01:34 +0200

    Add files via upload

---
+++ User Interface Design/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace User_Interface_Design
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            btnAdd = new Button();
+            btnDelete = new Button();
+            btnFind = new Button();
+            txtCode = new TextBox();
+            label2 = new Label();
+            txtMake = new TextBox();
+            txtQuantity = new TextBox();
+            label3 = new Label();
+            txtOutput = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(310, 142);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Mobile Code";
+            label1.Click += label1_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.BackColor = Color.Lime;
+            btnAdd.Location = new Point(169, 344);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            btnDelete.BackColor = Color.Red;
+            btnDelete.Location = new Point(332, 344);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 2;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnFind
+            // 
+            btnFind.BackColor = SystemColors.MenuHighlight;
+            btnFind.Location = new Point(487, 344);
+            btnFind.Name = "btnFind";
+            btnFind.Size = new Size(94, 29);
+            btnFind.TabIndex = 3;
+            btnFind.Text = "Find";
+            btnFind.UseVisualStyleBackColor = false;
+            // 
+            // txtCode
+            // 
+            txtCode.Location = new Point(433, 139);
+            txtCode.Name = "txtCode";
+            txtCode.Size = new Size(125, 27);
+            txtCode.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(310, 198);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Make";
+            // 
+            // txtMake
+            // 
+            txtMake.Location = new Point(433, 198);
+            txtMake.Name = "txtMake";
+            txtMake.Size = new Size(125, 27);
+            txtMake.TabIndex = 6;
+            // 
+            // txtQuantity
+            // 
+            txtQuantity.Location = new Point(433, 259);
+            txtQuantity.Name = "txtQuantity";
+            txtQuantity.Size = new Size(125, 27);
+            txtQuantity.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(310, 262);
+            label3.Name = "label3";
+            label3.Size = new Size(65, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Quantity";
+            // 
+            // txtOutput
+            // 
+            txtOutput.Location = new Point(169, 39);
+            txtOutput.Name = "txtOutput";
+            txtOutput.Size = new Size(412, 27);
+            txtOutput.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtOutput);
+            Controls.Add(label3);
+            Controls.Add(txtQuantity);
+            Controls.Add(txtMake);
+            Controls.Add(label2);
+            Controls.Add(txtCode);
+            Controls.Add(btnFind);
+            Controls.Add(btnDelete);
+            Controls.Add(btnAdd);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnAdd;
+        private Button btnDelete;
+        private Button btnFind;
+        private TextBox txtCode;
+        private Label label2;
+        private TextBox txtMake;
+        private TextBox txtQuantity;
+        private Label label3;
+        private TextBox txtOutput;
+    }
+}
